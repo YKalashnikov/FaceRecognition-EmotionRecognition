7d9ec05d616205e44d5a2e6e22a8f640db638fa1
YKalashnikov <uk2709@gmail.com>
2019-08-30 00:08:49 -0400

    first

---
+++ script.js
@@ -0,0 +1,41 @@
+
+const video = document.getElementById('video')
+//Connecting the front camera
+
+const startVideo = () => {
+    navigator.getUserMedia(
+        { video: {} },
+        stream => video.srcObject = stream,
+        err => console.log('err',err)
+    )
+}
+
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+   /*  faceapi.nets.ageGenderNet.loadFromUri('/models') */
+
+]).then(startVideo)
+
+
+
+video.addEventListener('play', ()=>{
+    const canvas = faceapi.createCanvasFromMedia(video)
+    document.body.append(canvas);
+    const displaySize = {width: video.width, height: video.height}
+    faceapi.matchDimensions(canvas, displaySize)
+   setInterval(async() => {
+     const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()/* .withAgeAndGender() */
+/*      console.log(faceapi.draw) */
+    const resizeDetections = faceapi.resizeResults(detections, displaySize)
+    canvas.getContext('2d').clearRect(0,0, canvas.width, canvas.height)
+    faceapi.draw.drawDetections(canvas, resizeDetections)
+    faceapi.draw.drawFaceLandmarks(canvas, resizeDetections)
+
+  /*   faceapi.draw.drawFaceExpressions(canvas, resizeDetections) */
+  /*  console.log(resizeDetections[0].age) */
+   }, 100)
+})
+
